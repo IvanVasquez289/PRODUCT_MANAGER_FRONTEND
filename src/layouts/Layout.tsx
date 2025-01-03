7969b82eee37ae439db10d298586c4dcb299b2c2
ivan <ivanjv1234@gmail.com>
2025-01-03 17:47:53 -0600

    feat: styles to Layout.tsx

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,8 +3,17 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-        <div>Layout</div>
-        <Outlet/>
+      <header className="bg-slate-800">
+        <div className="mx-auto max-w-6xl py-10">
+          <h1 className="text-4xl text-white font-extrabold">
+            Administrador de productos
+          </h1>
+        </div>
+      </header>
+
+      <main className="max-w-6xl mx-auto mt-10 p-10 bg-white shadow-md">
+        <Outlet />
+      </main>
     </>
-  )
+  );
 }
