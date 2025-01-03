7b393469e8e21e04d44ad5f6d8e66d0a322d9e27
ivan <ivanjv1234@gmail.com>
2025-01-03 17:59:37 -0600

    feat: Add routing between psges

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom"
 
 const NewProduct = () => {
   return (
-    <div>NewProduct</div>
+    <div className="flex justify-between"> 
+      <h2 className="text-4xl text-slate-500 font-black">Registrar producto</h2>
+      <Link
+        to={'/'}
+        className="text-white bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded-md"
+      >
+        Volver al listado de productos
+      </Link>
+    </div>
   )
 }
 
--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom"
+
 const Products = () => {
   return (
-    <div>Products</div>
+    <div className="flex justify-between"> 
+      <h2 className="text-4xl text-slate-500 font-black">Listado de productos</h2>
+      <Link
+        to={'/productos/nuevo'}
+        className="text-white bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded-md"
+      >
+        Registrar producto
+      </Link>
+    </div>
   )
 }
 
